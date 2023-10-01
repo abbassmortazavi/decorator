67bdb2da96e108b64cd9608d74a1bb9128a49398
Abbass <abbassmortazavi@gmail.com>
2023-10-01 19:27:40 +0330

    add swagger

--- decorator/app/Domain/Product/Routes/web.php
+++ decorator/app/Domain/Product/Routes/web.php
@@ -1,14 +1,2 @@
 <?php
 
-use App\Domain\Home\Http\Controllers\Web\HomeController;
-use App\Domain\Home\Http\Controllers\Web\OptionController;
-use Illuminate\Support\Facades\Route;
-/**
- *  home backend
- */
-Route::prefix('backend')
-    ->as('backend.')
-   // ->middleware(['auth:web'])
-    ->group(static function () {
-        Route::get('test', [HomeController::class, 'admin'])->name('home.admin');
-    });
