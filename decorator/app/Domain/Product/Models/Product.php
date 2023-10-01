ed62adc07c44d7fb71b357ea6ca4a996edbf6b17
Abbass <abbassmortazavi@gmail.com>
2023-10-01 19:43:40 +0330

    remove additional attributes

--- decorator/app/Domain/Product/Models/Product.php
+++ decorator/app/Domain/Product/Models/Product.php
@@ -6,8 +6,6 @@ use App\Infrastructure\Models\BaseModel;
 
 class Product extends BaseModel
 {
-    protected $fillable = ['position', 'priority', 'image', 'middle_content_title', 'middle_content', 'footer_content'];
 
-    protected $appends = ['updated_at_affix', 'created_at_affix'];
 
 }
